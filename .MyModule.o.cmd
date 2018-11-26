43b9cf00b2d690c18ade74684de8c7d754ea9908
princeontrojanhorse <princeontrojanhorse@gmail.com>
2018-11-26 13:31:08 +0900

    remove files

--- .MyModule.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/Projects/kmod/MyModule.o := ld -EL    -r -o /home/pi/Projects/kmod/MyModule.o /home/pi/Projects/kmod/my_module.o 
--- .tmp_versions/MyModule.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/Projects/kmod/MyModule.ko
-/home/pi/Projects/kmod/my_module.o
-
